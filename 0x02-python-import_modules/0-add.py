b4d8ffa083a50dc993f3b1dee4d72c6b1993ced9
Mohamed hanana <mohammedhanana1@gmail.com>
2024-01-04 16:05:48 -0800

    1st task

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
 a = 1
 b = 2
-add_0 = __import__("add_0")
-print("{} + {} = {}".format(a, b, add_0.add(a, b)))
+print("{} + {} = {}".format(a, b, add(a, b)))
